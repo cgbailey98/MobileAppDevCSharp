d55db539af4a7bd0de9da04f882435282e6ede39
Clayton Bailey <claybai98@gmail.com>
2024-11-25 12:50:15 -0600

    Having 'None' as an option in the Assessment type picker allowed more than 2 assessments to be added to a course. 'None' has been removed as an option. Tested the functionality and only 2 tests, one of each type, is allowed to be added to a course. This completes project requirement C3.

--- C971/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/C971.AssemblyInfo.cs
+++ C971/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/C971.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C971")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+32a538c8d40018bfb6f27582fa23ee24c55f535f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0d64410814d4da5ba2ef6507e6c11bab547155b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("C971")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C971")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
